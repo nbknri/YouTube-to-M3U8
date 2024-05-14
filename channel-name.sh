692c3012413a3a489c55893f390c0c69c09214d7
nbknri <nbknri@gmail.com>
2024-05-15 04:45:34 +0530

    Update channel-name.sh

--- channel-name.sh
+++ channel-name.sh
@@ -4,16 +4,8 @@ echo $(dirname $0)
 
 python3 -m pip install requests
 
-cd $(dirname $0)
+cd $(dirname $0)/scripts/
 
-mkdir -p m3u8_files
-
-cd scripts
-
-while IFS= read -r line; do
-    if [[ $line != https:* ]]; then
-        python3 channel-name.py > "../m3u8_files/$line.m3u8"
-    fi
-done < ../channel-name.txt
+python3 channel-name.py
 
 echo m3u8 files generated

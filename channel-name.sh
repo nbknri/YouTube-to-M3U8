ee24b187a6af36bac26ec15a882d53a0614ab286
nbknri <nbknri@gmail.com>
2024-05-15 00:57:21 +0530

    Update channel-name.sh

--- channel-name.sh
+++ channel-name.sh
@@ -6,14 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 channel-name.py > ../channel-name1.m3u8
+python3 channel-name.py
 
-echo m3u8 grabbed for channel 1
-
-python3 channel-name.py > ../channel-name2.m3u8
-
-echo m3u8 grabbed for channel 2
-
-python3 channel-name.py > ../channel-name3.m3u8
-
-echo m3u8 grabbed for channel 3
+echo m3u8 files generated

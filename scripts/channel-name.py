d59e4d4a92045bfdc4dd1595695561559b60b7c9
nbknri <nbknri@gmail.com>
2024-05-15 09:06:59 +0530

    Update channel-name.py

--- scripts/channel-name.py
+++ scripts/channel-name.py
@@ -12,22 +12,23 @@ def grab(url, channel_name, group_name, logo, output_folder):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                link = 'https://raw.githubusercontent.com/nbknri/YouTube-to-M3U8/main/assets/info.m3u8'
+                print('https://raw.githubusercontent.com/nbknri/YouTube-to-M3U8/main/assets/info.m3u8')
                 output_file = os.path.join(output_folder, f'{channel_name.replace(" ", "")}.m3u8')
                 with open(output_file, 'w') as f:
                     f.write(f'#EXTM3U\n')
                     f.write(f'#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n')
-                    f.write(link)
+                    f.write('https://raw.githubusercontent.com/nbknri/YouTube-to-M3U8/main/assets/info.m3u8')
                 return
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                link = 'https://raw.githubusercontent.com/nbknri/YouTube-to-M3U8/main/assets/info.m3u8'
+                print('https://raw.githubusercontent.com/nbknri/YouTube-to-M3U8/main/assets/info.m3u8')
                 output_file = os.path.join(output_folder, f'{channel_name.replace(" ", "")}.m3u8')
                 with open(output_file, 'w') as f:
-                    f.write(f'#EXTM3U\n')
-                    f.write(f'#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n')
-                    f.write(link)
+                    f.write('#EXTM3U\n')
+                    f.write('#EXT-X-VERSION:3\n')
+                    f.write('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000\n')
+                    f.write('https://raw.githubusercontent.com/nbknri/YouTube-to-M3U8/main/assets/info.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
@@ -56,11 +57,6 @@ output_folder = '../channel'
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# Create m3u8 playlist
-master_playlist = os.path.join(output_folder, '../playlist.m3u')
-with open(master_playlist, 'w') as master:
-    master.write('#EXTM3U\n')
-
 with open('../channel-name.txt') as f:
     lines = f.readlines()
     for i, line in enumerate(lines):
@@ -68,12 +64,5 @@ with open('../channel-name.txt') as f:
         if '|' in line:
             channel_info = line.split(' | ')
             name = channel_info[0]
-            group_name = channel_info[1]
-            logo = channel_info[2]
             url = lines[i+1].strip()  # Get the URL from the next line
-            grab(url, name, group_name, logo, output_folder)
-            # Append channel info to master playlist
-            m3u8_file = f'https://raw.githubusercontent.com/nbknri/YouTube-to-M3U8/main/channel/{name.replace(" ", "")}.m3u8'
-            with open(master_playlist, 'a') as master:
-                master.write(f'#EXTINF:-1 group-title="{group_name}" tvg-logo="{logo}", {name}\n')
-                master.write(f'{m3u8_file}\n')
+            grab(url, name, output_folder)
